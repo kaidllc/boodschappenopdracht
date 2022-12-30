c0f4489d22e2298b8e7b775804a2aa457c5d8f35
kaidllc <senndllc@gmail.com>
2022-12-30 15:00:47 +0100

    created first files

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Http\Controllers\GroceryController;
+
+Route::get('/groceries', [GroceriesController::class, 'index'])->name('groceries.index');
+
+Route::get('/groceries/create', [GroceriesController::class, 'create'])->name('groceries.create');
+
+Route::post('/groceries', [GroceriesController::class, 'store'])->name('groceries.store');
+
+Route::get('/groceries/{grocery}/edit', [GroceriesController::class, 'edit'])->name('groceries.edit');
+
+Route::put('/groceries/{grocery}', [GroceriesController::class, 'update'])->name('groceries.update');
+
+Route::delete('/groceries/{grocery}', [GroceriesController::class, 'destroy'])->name('groceries.destroy');
+
+
+//https://laravel.com/docs/9.x/controllers
